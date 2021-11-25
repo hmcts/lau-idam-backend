9a96ee86612bf0e1aff01279c4ce8c0b8d041380
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2021-11-25 11:22:05 +0000

    Logon Post API functional tests changed s2s token alignment

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityFunctionalTests/model/LogOnPostResponseVO.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityFunctionalTests/model/LogOnPostResponseVO.java
@@ -28,21 +28,17 @@ public class LogOnPostResponseVO {
         return id;
     }
 
-
     public void setId(String id) {
         this.id = id;
     }
-
 
     public String getUserId() {
         return userId;
     }
 
-
     public void setUserId(String userId) {
         this.userId = userId;
     }
-
 
     public String getEmailAddress() {
         return emailAddress;
