1d1d6c3e24272e88f7e08af2d4057d55d86c951d
VamshiRudrabhatlaHMCTS <vamshi.rudrabhatla@HMCTS.net>
2021-12-06 11:06:57 +0000

    changed S2S_NAME

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityfunctionaltests/utils/TestConstants.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityfunctionaltests/utils/TestConstants.java
@@ -2,7 +2,7 @@ package uk.gov.hmcts.reform.laubackend.idam.serenityfunctionaltests.utils;
 
 public final class TestConstants {
 
-    public static final String S2S_NAME = "lau-idam-frontend";
+    public static final String S2S_NAME = "lau_idam_frontend";
     public static final String S2S_URL = "http://rpe-service-auth-provider-aat.service.core-compute-aat.internal/testing-support";
 
     /*Logon endPoint*/
