be14a82afd98e3a6d7928e2b323514a6abdc0567
Martin Spasov <martin.y.spasov@gmail.com>
2022-01-27 11:28:33 +0000

    LAU-249 Delete all test data from AAT (#77)

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityfunctionaltests/model/LogonLogPostResponseVO.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.laubackend.idam.serenityfunctionaltests.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class LogonLogPostResponseVO {
+    private LogonLogVO logonLog;
+}
