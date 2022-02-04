49885c08ed7162ed3dd49824438b9300b320e02e
Dave Jones <dave.jones@hmcts.net>
2022-02-04 14:23:04 +0000

    LAU-258: Correct scripts.

--- src/main/resources/db/migration/V003__Schema.sql
+++ src/main/resources/db/migration/V003__Schema.sql
@@ -1,1 +1,16 @@
-update idam_logon_audit set email_address = lower(email_address);
+ALTER TABLE idam_logon_audit ALTER COLUMN email_address TYPE VARCHAR(256);
+
+-- drop extension safely
+DO
+$do$
+BEGIN
+  IF EXISTS (
+  SELECT FROM pg_extension  -- SELECT list can be empty for this
+    WHERE extname = 'citext') THEN
+  DROP EXTENSION IF EXISTS citext;
+END IF;
+exception
+  when sqlstate '42501' then
+  RAISE NOTICE 'Dropping citext failed.';
+END
+$do$;
