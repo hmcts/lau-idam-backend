bea5b458eed213d4744c3032a99341c382ddfbbe
Dave Jones <38425925+davejones74@users.noreply.github.com>
2022-02-15 15:58:58 +0000

    Minor update to correct Update script.

--- src/main/resources/db/migration/V004__Schema.sql
+++ src/main/resources/db/migration/V004__Schema.sql
@@ -11,6 +11,6 @@ comment on column idam_logon_audit.email_address_mac is 'Encoded email search st
 -- Update script to copy existing email addresses values over
 UPDATE idam_logon_audit ila
 SET email_address_mac = encode(hmac(ila2.email, '${LAU_IDAM_ENCRYPTION_KEY}', 'sha256'), 'hex')
-FROM (SELECT id, pgp_sym_decrypt(decode(ila2.email_address_mac, 'base64'), '${LAU_IDAM_ENCRYPTION_KEY}') AS email
+FROM (SELECT id, pgp_sym_decrypt(decode(ila2.email_address, 'base64'), '${LAU_IDAM_ENCRYPTION_KEY}') AS email
       FROM idam_logon_audit ila2) ila2
 WHERE ila.id = ila2.id;
