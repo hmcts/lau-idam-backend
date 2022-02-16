84a5996aba2dd28564429603c0d8db0eed4f3979
Dave Jones <dave.jones@hmcts.net>
2022-02-16 00:26:34 +0000

    LAU-282: Update to remove data migration.

--- src/main/resources/db/migration/V004__Schema.sql
+++ src/main/resources/db/migration/V004__Schema.sql
@@ -8,9 +8,9 @@ CREATE INDEX idam_logon_audit_enc_str1_idx ON idam_logon_audit (email_address_ma
 -- Comments case_search_audit for new column email_address_mac
 comment on column idam_logon_audit.email_address_mac is 'Encoded email search string';
 
--- Update script to copy existing email addresses values over
-UPDATE idam_logon_audit ila
-SET email_address_mac = encode(hmac(ila2.email, '${LAU_IDAM_ENCRYPTION_KEY}', 'sha256'), 'hex')
-FROM (SELECT id, pgp_sym_decrypt(decode(ila2.email_address, 'base64'), '${LAU_IDAM_ENCRYPTION_KEY}') AS email
-      FROM idam_logon_audit ila2) ila2
-WHERE ila.id = ila2.id;
+-- Update script to copy existing email addresses values over (commented out as it needs LAU_IDAM_ENCRYPTION_KEY)
+--UPDATE idam_logon_audit ila
+--SET email_address_mac = encode(hmac(ila2.email, '[LAU_IDAM_ENCRYPTION_KEY]', 'sha256'), 'hex')
+--FROM (SELECT id, pgp_sym_decrypt(decode(ila2.email_address, 'base64'), '[LAU_IDAM_ENCRYPTION_KEY]') AS email
+--      FROM idam_logon_audit ila2) ila2
+--WHERE ila.id = ila2.id;
