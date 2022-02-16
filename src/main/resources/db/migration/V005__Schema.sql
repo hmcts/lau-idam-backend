5951d55efc4eda75cb7acf20edc3b9892fa9710b
Martin Spasov <martin.y.spasov@gmail.com>
2022-02-16 12:32:28 +0000

    Revert "Enable DB migration. (#96)" (#98)
    This reverts commit 3358c7bc53f207d624fe3e0bf34d6c180b386387.

--- src/main/resources/db/migration/V005__Schema.sql
+++
@@ -1,5 +0,0 @@
-UPDATE idam_logon_audit ila
-SET email_address_mac = encode(hmac(ila2.email, '${LAU_IDAM_ENCRYPTION_KEY}', 'sha256'), 'hex')
-FROM (SELECT id, pgp_sym_decrypt(decode(ila2.email_address, 'base64'), '${LAU_IDAM_ENCRYPTION_KEY}') AS email
-      FROM idam_logon_audit ila2) ila2
-WHERE ila.id = ila2.id;
