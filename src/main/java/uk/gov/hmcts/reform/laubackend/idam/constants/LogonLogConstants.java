16a4d828dbebf041f65341f6400275c7cada7fc2
Dave Jones <38425925+davejones74@users.noreply.github.com>
2022-03-29 13:56:34 +0100

    LAU-318 app insights telemetry (#117)
    * LAU-266: Update to add telemetry.
    
    * LAU-266: Correction to tests.
    
    * LAU-266: Update timimg before verification and improve tests.

--- src/main/java/uk/gov/hmcts/reform/laubackend/idam/constants/LogonLogConstants.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/idam/constants/LogonLogConstants.java
@@ -9,6 +9,12 @@ public final class LogonLogConstants {
     public static final String SIZE = "size";
     public static final String PAGE = "page";
 
+    public static final long PERF_TOLERANCE_THRESHOLD_MS = 1500;
+    public static final String PERF_THRESHOLD_MESSAGE_BELOW =
+        "Good: below threshold (" + PERF_TOLERANCE_THRESHOLD_MS + "ms)";
+    public static final String PERF_THRESHOLD_MESSAGE_ABOVE =
+        "Bad: above threshold (" + PERF_TOLERANCE_THRESHOLD_MS + "ms)";
+
     private LogonLogConstants() {
     }
 }
