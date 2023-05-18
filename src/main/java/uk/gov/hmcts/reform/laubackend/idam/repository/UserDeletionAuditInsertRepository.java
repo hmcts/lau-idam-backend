496e145201c2f240f765edcee212ceed93c5f96f
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-05-18 13:58:07 +0100

    LAU-634 added user deletion audit model

---
+++ src/main/java/uk/gov/hmcts/reform/laubackend/idam/repository/UserDeletionAuditInsertRepository.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.reform.laubackend.idam.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import uk.gov.hmcts.reform.laubackend.idam.domain.UserDeletionAudit;
+
+@Repository
+@RequiredArgsConstructor
+public class UserDeletionAuditInsertRepository {
+
+    private static final String INSERT_QUERY = """
+        INSERT INTO public.user_deletion_audit (
+            user_id,
+            email_address,
+            email_address_hmac,
+            first_name,
+            first_name_hmac,
+            last_name,
+            last_name_hmac,
+            deletion_timestamp
+        )
+        VALUES (
+            :userId,
+            encrypt_value(:emailAddress, :encryptionKey),
+            hash_value(:emailAddress, :encryptionKey),
+            encrypt_value(:firstName, :encryptionKey),
+            hash_value(:firstName, :encryptionKey),
+            encrypt_value(:lastName, :encryptionKey),
+            hash_value(:lastName, :encryptionKey),
+            :deletionTimestamp
+        ) RETURNING id
+        """;
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Transactional
+    public UserDeletionAudit saveUserDeleteAuditWithEncryption(
+        UserDeletionAudit userDeletionAudit,
+        String securityDbBackendEncryptionKey) {
+        Query insertQuery = entityManager.createNativeQuery(INSERT_QUERY);
+
+        insertQuery.setParameter("userId", userDeletionAudit.getUserId())
+            .setParameter("emailAddress", userDeletionAudit.getEmailAddress())
+            .setParameter("firstName", userDeletionAudit.getFirstName())
+            .setParameter("lastName", userDeletionAudit.getLastName())
+            .setParameter("deletionTimestamp", userDeletionAudit.getTimestamp())
+            .setParameter("encryptionKey", securityDbBackendEncryptionKey);
+
+        Integer generatedId = (Integer) insertQuery.getSingleResult();
+        userDeletionAudit.setId(generatedId.longValue());
+        return userDeletionAudit;
+    }
+
+}
