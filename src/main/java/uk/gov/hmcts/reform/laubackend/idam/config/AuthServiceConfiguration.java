d566f9d532366723f2f8bc65007afca94d8a2406
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-10-13 09:34:29 +0100

    reshuffle feign client registration

--- src/main/java/uk/gov/hmcts/reform/laubackend/idam/config/AuthServiceConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/idam/config/AuthServiceConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.laubackend.idam.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,11 +8,9 @@ import org.springframework.context.annotation.Lazy;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
 import uk.gov.hmcts.reform.authorisation.validators.ServiceAuthTokenValidator;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @Configuration
 @Lazy
-@EnableFeignClients(clients = IdamApi.class)
 @ComponentScan(basePackages = {"uk.gov.hmcts.reform.idam"})
 public class AuthServiceConfiguration {
 
