1fbde946dfd0b07255e1ea7a5b233471e432ce0f
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2023-11-06 11:46:30 +0000

    LAU-803 increase size for postgres flexible server

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -47,3 +47,9 @@ variable "postgresql_flexible_server_port" {
 }
 
 variable "aks_subscription_id" {}
+
+variable "pgsql_storage_mb" {
+  type = number
+  description = "Flexible Postgres DB size in mb"
+  default = 65536
+}
