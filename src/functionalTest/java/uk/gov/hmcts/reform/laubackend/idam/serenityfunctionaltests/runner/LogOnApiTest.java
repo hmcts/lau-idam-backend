398b3a9f3b2092919c205cd40d51e310798c7743
Martin <martin.spasov@hmcts.net>
2023-11-22 11:09:32 +0000

    Enable functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityfunctionaltests/runner/LogOnApiTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/laubackend/idam/serenityfunctionaltests/runner/LogOnApiTest.java
@@ -9,7 +9,6 @@ import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.annotations.Steps;
 import net.serenitybdd.annotations.Title;
 import org.json.JSONException;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.testng.Assert;
@@ -25,7 +24,6 @@ import java.util.Map;
 import static uk.gov.hmcts.reform.laubackend.idam.serenityfunctionaltests.helper.DatabaseCleaner.deleteLogonRecord;
 
 @RunWith(SerenityRunner.class)
-@Ignore
 public class LogOnApiTest {
 
 
