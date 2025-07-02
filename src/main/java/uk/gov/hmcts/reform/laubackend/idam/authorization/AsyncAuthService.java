a6bbf34283ff7643b40ab1f506afb71678d3571b
Martin <martin.spasov@hmcts.net>
2025-07-02 17:05:40 +0100

    Remove Async

--- src/main/java/uk/gov/hmcts/reform/laubackend/idam/authorization/AsyncAuthService.java
+++ src/main/java/uk/gov/hmcts/reform/laubackend/idam/authorization/AsyncAuthService.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.laubackend.idam.authorization;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.laubackend.idam.exceptions.InvalidServiceAuthorizationException;
 import uk.gov.hmcts.reform.laubackend.idam.feign.ServiceAuthorizationFeignClient;
@@ -18,7 +17,6 @@ public class AsyncAuthService {
         this.serviceAuthorizationFeignClient = serviceAuthorizationFeignClient;
     }
 
-    @Async("TaskExecutor")
     public CompletableFuture<String> authenticateService(final String authHeader) {
         if (authHeader != null) {
             return CompletableFuture.completedFuture(serviceAuthorizationFeignClient.getServiceName(authHeader));
