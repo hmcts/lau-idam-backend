e62d1b8573d01bcce2d121b3e8099bba8863e502
Priyanka Verma <priyanka.verma@hmcts.net>
2026-03-12 11:54:22 +0000

    remove discovery information

--- src/smokeTest/java/uk/gov/hmcts/reform/laubackend/idam/smoke/SmokeTests.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/laubackend/idam/smoke/SmokeTests.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
